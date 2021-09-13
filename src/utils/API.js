7d980084cd00cd1e64b3fcc2fe4ef41663ee74f4
Abdulrahman <boodynaser@rocketmail.com>
2021-09-13 16:09:41 +0200

    Fetch From API

---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+const fetch = require('cross-fetch');
+
+const fetchcorona = async (country) => {
+  const resp = await fetch(`https://api.covid19tracking.narrativa.com/api/2020-03-22/country/${country}`);
+  const respobj = await resp.json();
+  console.log(respobj);
+};
+
+fetchcorona('egypt');
+
+// export default fetchcorona;
