ab211f86acf661bfb6eca36e25fed9a99274edf7
Abdulrahman <boodynaser@rocketmail.com>
2021-09-13 18:50:41 +0200

    add country shapes

--- src/components/countrycomponent.js
+++ src/components/countrycomponent.js
@@ -2,11 +2,12 @@
 /* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
 import style from './countrycomponent.module.css';
+import flag from '../assets/country.svg';
 
 const Country = (props) => (
   <Link to="/description" className={style.Link}>
-    <div className={style.container}>
-      <img src="sdfsf" alt={props.country} />
+    <div className={style.container1}>
+      <img src={flag} alt={props.country} />
       <h1>
         {props.country}
       </h1>
--- src/components/homecomponent.js
+++ src/components/homecomponent.js
@@ -1,15 +1,12 @@
-// import {
-//   Link,
-// } from 'react-router-dom';
 import Country from './countrycomponent';
 
 const Home = () => (
   <>
     <h1>Stats By Country</h1>
-    <Country country="Egypt" today_confirmed="383" />
-    <Country country="German" today_confirmed="69" />
-    <Country country="Italy" today_confirmed="9483" />
-    <Country country="Spain" today_confirmed="655" />
+    <Country country="Egypt" styleclass="container1" today_confirmed="383" />
+    <Country country="German" styleclass="container2" today_confirmed="69" />
+    <Country country="Italy" styleclass="container1" today_confirmed="9483" />
+    <Country country="Spain" styleclass="container2" today_confirmed="655" />
   </>
 );
 
