e0432ad0f20621b218e5072e676c5826db5e0847
Abdulrahman <boodynaser@rocketmail.com>
2021-09-13 23:42:48 +0200

    Use state component for each country description

--- src/components/detailscomponent.js
+++ src/components/detailscomponent.js
@@ -1,7 +1,7 @@
 import {
   Link,
 } from 'react-router-dom';
-import Country from './countrycomponent';
+import State from './statecomponent';
 import style from './detailscomponent.module.css';
 import backarrow from '../assets/back-arrow.svg';
 
@@ -10,13 +10,13 @@ const Details = () => (
     <Link exact to="/">
       <img src={backarrow} alt="backarrow" className={style.backarrow} />
     </Link>
-    <Country class="even" nav={false} country="Germany" styleclass="container1" today_confirmed="383" />
+    <State class="even" nav={false} country="Germany" styleclass="container1" today_confirmed="383" />
     <h3>CITY/TOWN BREAKDOWN</h3>
     <div className={style.container}>
-      <Country class="even" nav country="Munich" styleclass="container1" today_confirmed="383" />
-      <Country class="odd" nav country="Hamburg" styleclass="container2" today_confirmed="69" />
-      <Country class="even" nav country="Berlin" styleclass="container1" today_confirmed="9483" />
-      <Country class="odd" nav country="Koln" styleclass="container2" today_confirmed="655" />
+      <State class="even" nav country="Munich" styleclass="container1" today_confirmed="383" />
+      <State class="odd" nav country="Hamburg" styleclass="container2" today_confirmed="69" />
+      <State class="even" nav country="Berlin" styleclass="container1" today_confirmed="9483" />
+      <State class="odd" nav country="Koln" styleclass="container2" today_confirmed="655" />
     </div>
   </div>
 );
---
+++ src/components/statecomponent.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
+import style from './statecomponent.module.css';
+import navarrow from '../assets/right-arrow.svg';
+
+const State = (props) => {
+  const stylex = props.class === 'even' ? style.country1 : style.country2;
+  let navarrowicon = '';
+  if (props.nav) {
+    navarrowicon = (
+      <img src={navarrow} alt="navarrow" />
+    );
+  }
+  return (
+
+    <div className={stylex}>
+      <div className={style.textcont}>
+        <h1>
+          {props.country}
+        </h1>
+        <h3>
+          {props.today_confirmed}
+          {navarrowicon}
+        </h3>
+      </div>
+    </div>
+
+  );
+};
+
+export default State;
