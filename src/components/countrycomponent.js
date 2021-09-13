532270f03032c5a730ec2ae3f25b78578c764dda
Abdulrahman <boodynaser@rocketmail.com>
2021-09-13 19:47:59 +0200

    Use grid to style home page

--- src/components/countrycomponent.js
+++ src/components/countrycomponent.js
@@ -3,12 +3,14 @@
 import { Link } from 'react-router-dom';
 import style from './countrycomponent.module.css';
 import flag from '../assets/country.svg';
+import navarrow from '../assets/right-arrow.svg';
 
 const Country = (props) => {
   const stylex = props.class === 'even' ? style.country1 : style.country2;
   return (
     <Link to="/description" className={style.Link}>
       <div className={stylex}>
+        <img src={navarrow} alt="navarrow" />
         <img src={flag} alt={props.country} />
         <h1>
           {props.country}
--- src/components/homecomponent.js
+++ src/components/homecomponent.js
@@ -2,7 +2,7 @@ import Country from './countrycomponent';
 import style from './homecomponent.module.css';
 
 const Home = () => (
-  <>
+  <div className={style.home}>
     <h1>Stats By Country</h1>
     <div className={style.container}>
       <Country class="even" country="Egypt" styleclass="container1" today_confirmed="383" />
@@ -10,7 +10,7 @@ const Home = () => (
       <Country class="odd" country="Italy" styleclass="container1" today_confirmed="9483" />
       <Country class="even" country="Spain" styleclass="container2" today_confirmed="655" />
     </div>
-  </>
+  </div>
 );
 
 export default Home;
