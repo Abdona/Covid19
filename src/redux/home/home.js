eade3ee5e17ade9456a2505f9d51de618a51ae88
Abdulrahman <boodynaser@rocketmail.com>
2021-09-14 16:40:52 +0200

    build home page redux action and reducer

---
+++ src/redux/home/home.js
@@ -0,0 +1,19 @@
+const ADD_STATS = 'covid19/home/ADD_STATS';
+
+const initialState = [];
+
+export const addAction = (payload) => ({
+  type: ADD_STATS,
+  payload,
+});
+
+const homeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_STATS:
+      return [...state, ...action.payload];
+    default:
+      return state;
+  }
+};
+
+export default homeReducer;
--- src/utils/API.js
+++ src/utils/API.js
@@ -3,9 +3,7 @@ const fetch = require('cross-fetch');
 const fetchcorona = async (country) => {
   const resp = await fetch(`https://api.covid19tracking.narrativa.com/api/2020-03-22/country/${country}`);
   const respobj = await resp.json();
-  console.log(respobj.total.today_confirmed);
+  return respobj;
 };
 
-fetchcorona('germany');
-
-// export default fetchcorona;
+export default fetchcorona;
