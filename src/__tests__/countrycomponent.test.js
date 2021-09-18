2a810caab0eeb70f38ba605b86a1fae80fb9287d
Abdulrahman <boodynaser@rocketmail.com>
2021-09-18 10:38:53 +0200

    Test real country component instead of mocked

--- src/__tests__/countrycomponent.test.js
+++ src/__tests__/countrycomponent.test.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import ReactDOM from 'react-dom';
+import store from '../redux/configureStore';
 import Country from '../components/countrycomponent';
 import '@testing-library/jest-dom';
 
 describe('Test Home page Components', () => {
   it('test HomePage function component renders without error', () => {
     const divElement = document.createElement('div');
-    ReactDOM.render(<Country />, divElement);
+    ReactDOM.render(<Provider store={store}><Country /></Provider>, divElement);
   });
 
   it('render correctly', () => {
-    const { getByTestId } = render(<Country country="Egypt" today_confirmed="239" />);
+    const { getByTestId } = render(<Provider store={store}><Country country="Egypt" today_confirmed="239" /></Provider>);
     expect(getByTestId('country')).toHaveTextContent('Egypt239');
   });
 
   it('matches snapshot', () => {
-    const tree = renderer.create(<Country />).toJSON();
+    const tree = renderer.create(<Provider store={store}><Country /></Provider>).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
